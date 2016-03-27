579261ca35e75f1780d743f24223e171daf2a018
Mahmoud Tantawy <mahmoud.tantawy@robustastudio.com>
2016-03-27 23:27:48 +0200

    Trying to avoid notifications for bots, also adding more checks/validations

--- pushbullet-visitors-notification.php
+++ pushbullet-visitors-notification.php
@@ -13,22 +13,22 @@ namespace Mtantawy\PushbulletVisitorsNotification;
 // if u want to load a function only in the front end
 add_action('wp_loaded', function () {
     if (!is_admin()) { // Only target the front end
-        $access_token = get_option('mtantawy_pushbullet_access_token');
+        if (checkUserAgent()) {
+            return false;
+        }
         $ip = getVisitorIp() ?: false;
         if (false === $ip) {
             return false;
         }
         $path = plugin_dir_path(__FILE__);
-
+        $access_token = get_option('mtantawy_pushbullet_access_token');
+        
         exec('cd "'.$path.'"; php cli.php '.$access_token.' '.$ip.' >> output');
     }
 });
 
 function getVisitorIp()
 {
-    if (false !== strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'jetpack')) {
-        return false;
-    }
     $ip = $_SERVER['REMOTE_ADDR'];
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {               // check ip from share internet
         $ip = $_SERVER['HTTP_CLIENT_IP'];
@@ -37,5 +37,16 @@ function getVisitorIp()
     }
     $ips = explode(',', $ip);
 
-    return $ips[0];
+    return isset($ips[0]) ? $ips[0] : false;
+}
+
+function checkUserAgent()
+{
+    $user_agent_strings = ['jetpack', 'bot', 'wordpress'];
+    foreach ($user_agent_strings as $string) {
+        if (false !== strpos(strtolower($_SERVER['HTTP_USER_AGENT']), $string)) {
+            return false;
+        }
+    }
+    return true;
 }
